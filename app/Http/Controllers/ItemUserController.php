06f723cb57e71d49109a624231872fb96d40c3a1
taigamiyazato <gonglitaifu@gmail.com>
2017-12-15 11:34:14 +0000

    want

--- app/Http/Controllers/ItemUserController.php
+++ app/Http/Controllers/ItemUserController.php
@@ -7,11 +7,85 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Item;
-
 class ItemUserController extends Controller
 {
-    public function want()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+     public function want()
     {
         $itemCode = request()->itemCode;
 
@@ -33,8 +107,6 @@ class ItemUserController extends Controller
         ]);
 
         \Auth::user()->want($item->id);
-        \Auth::user()->have($item->id);
-
 
         return redirect()->back();
     }
@@ -49,7 +121,33 @@ class ItemUserController extends Controller
         }
         return redirect()->back();
     }
-    
+
+     public function have()
+    {
+        $itemCode = request()->itemCode;
+
+        // itemCode から商品を検索
+        $client = new \RakutenRws_Client();
+        $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+        $rws_response = $client->execute('IchibaItemSearch', [
+            'itemCode' => $itemCode,
+        ]);
+        $rws_item = $rws_response->getData()['Items'][0]['Item'];
+
+        // Item 保存 or 検索（見つかると作成せずにそのインスタンスを取得する）
+        $item = Item::firstOrCreate([
+            'code' => $rws_item['itemCode'],
+            'name' => $rws_item['itemName'],
+            'url' => $rws_item['itemUrl'],
+            // 画像の URL の最後に ?_ex=128x128 とついてサイズが決められてしまうので取り除く
+            'image_url' => str_replace('?_ex=128x128', '', $rws_item['mediumImageUrls'][0]['imageUrl']),
+        ]);
+
+        \Auth::user()->have($item->id);
+
+        return redirect()->back();
+    }
+
     public function dont_have()
     {
         $itemCode = request()->itemCode;
--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,4 +1,30 @@
-public function create()
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
         $keyword = request()->keyword;
         $items = [];
@@ -28,10 +54,27 @@ public function create()
             'items' => $items,
         ]);
     }
-    
-public function show($id)
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-      $item = Item::find($id);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
       $want_users = $item->want_users;
       $have_users = $item->have_users;
 
@@ -41,3 +84,38 @@ public function show($id)
           'have_users' => $have_users,
       ]);
     }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
